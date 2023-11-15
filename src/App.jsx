a3853ca6d8e00e9b5f02932a309d3e7ce7d0ad5a
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-15 18:20:14 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Typed from "react-typed";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -24,12 +25,30 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <div className="about-mobile-container">
+          <Typed
+            className="typed-text-about"
+            strings={[
+              `Je m'appelle Guillaume Leroy. Développeur Web Junior à Lille, j'ai 28 ans. Suite à une
+            reconversion de carrière, j'ai décidé de me lancer dans le
+            développement web. J'ai suivi une formation et des cours en ligne pour maîtriser les
+            langages de programmation, notamment HTML, CSS, JavaScript, ainsi
+            que des frameworks populaires tels que React et NodeJS. <br />
+            Mon ambition est de m'investir dans des projets stimulants,
+            d'acquérir une expérience pratique significative, et de participer
+            à la création de solutions créatives et innovantes. Je suis
+            actuellement ouvert aux opportunités de stage ou d'emploi dans le
+            domaine du développement web.`,
+            ]}
+            typeSpeed={1}
+          />
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
