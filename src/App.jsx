b54c97d959aeb9638bf56663eb238a030452710c
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-23 11:52:32 +0100

    testcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from "react";
 import "./App.scss";
-import Darkmode from "darkmode-js";
 
 const App = () => {
-  new Darkmode().showWidget();
-  const options = {
-    bottom: "64px", // default: '32px'
-    right: "unset", // default: '32px'
-    left: "32px", // default: 'unset'
-    time: "0.5s", // default: '0.3s'
-    mixColor: "#fff", // default: '#fff'
-    backgroundColor: "#fff", // default: '#fff'
-    buttonColorDark: "#100f2c", // default: '#100f2c'
-    buttonColorLight: "#fff", // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: "🌓", // default: ''
-    autoMatchOsTheme: true, // default: true
-  };
-
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-
   return (
     <div className="nes-container is-dark">Bienvenue sur mon folio test</div>
   );
